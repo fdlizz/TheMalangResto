37e51faa9d33d6b3527137060c5e9472917208a1
fdlizz <fadelizzat123@gmail.com>
2024-04-24 10:45:12 +0700

    update

--- HomePage.php
+++ HomePage.php
@@ -6,30 +6,62 @@ include "data.php";
 <body>
     <div>
         <div class="home">
-            <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="..." class="d-block w-100" alt="...">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="..." class="d-block w-100" alt="...">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="..." class="d-block w-100" alt="...">
+            <div>
+                <img class="opacity-100 fade" src="photo/fade.png" alt="">
+            </div>
+            <!-- <div class="slide1">
+                <div class="slider">
+                    <div class="slides">
+                        <input type="radio" name="radio-button" id="radio1">
+                        <input type="radio" name="radio-button" id="radio2">
+                        <input type="radio" name="radio-button" id="radio3">
+                        <input type="radio" name="radio-button" id="radio4">
+
+                        <div class="slide first">
+                            <img src="photo/Menu Baru1.png" alt="pict">
+                        </div>
+                        <div class="slide">
+                            <img src="photo/logo.jpeg" alt="pict">
+                        </div>
+                        <div class="slide">
+                            <img src="photo/Menu Baru1.png" alt="pict">
+                        </div>
+                        <div class="slide">
+                            <img src="photo/logo.jpeg" alt="pict">
+                        </div>
+
+                        <div class="nav-auto">
+                            <div class="btn-auto1"></div>
+                            <div class="btn-auto2"></div>
+                            <div class="btn-auto3"></div>
+                            <div class="btn-auto4"></div>
+                        </div>
                     </div>
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
-            </div>
+
+                <div class="nav-manual">
+                    <label for="radio1" class="manual-btn"></label>
+                    <label for="radio2" class="manual-btn"></label>
+                    <label for="radio3" class="manual-btn"></label>
+                    <label for="radio4" class="manual-btn"></label>
+                </div>
+            </div> -->
         </div>
-        <div class="Menu1">
-            teks & gambar
+    </div>
+
+    <div class="Menu1">
+        <div class="rekomend">
+            <h1>Rekomendasi</h1>
         </div>
+        <div class="makanan">
+            <h1>Makanan</h1>
+        </div>
+        <div class="minuman">
+            <h1>Minuman</h1>
+        </div>
+        <div class="snack">
+            <h1>Snack</h1>
+        </div>
+    </div>
     </div>
 </body>
