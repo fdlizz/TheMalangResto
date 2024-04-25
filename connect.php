dd5545a5f3d5e95190988a6a54d032ae3a69af74
fdlizz <fadelizzat123@gmail.com>
2024-04-25 09:19:52 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,17 +1,16 @@
 <?php
-    $serverName = "localhost";
-    $userName = "root";
-    $password = "";
-    $database = "the malang resto";
+$serverName = "localhost";
+$userName = "root";
+$password = "";
+$database = "the malang resto";
 
-    //buat koneksi
-    $koneksi = mysqli_connect($serverName, $userName, $password, $database);
+//buat koneksi
+$koneksi = mysqli_connect($serverName, $userName, $password, $database);
 
-    // cek koneksi
-    // if (!$koneksi) {
-    //     die("Koneksi Gagal".mysqli_connect_error());
-    // }
-    // else{
-    //     echo "koneksi berhasil";
-    // }
-?>
+// cek koneksi
+// if (!$koneksi) {
+//     die("Koneksi Gagal".mysqli_connect_error());
+// }
+// else{
+//     echo "koneksi berhasil";
+// }
